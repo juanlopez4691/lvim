3342f34b2265e427ac789e06319e6c3f48653ee7
Joan Lopez <juanlopez4691@gmail.com>
2021-08-14 13:06:50 +0200

    Temporarily removed JS, TS, PHP linters and formatters.

--- custom/settings.lua
+++ custom/settings.lua
@@ -17,25 +17,25 @@ vim.cmd("set whichwrap+=<,>,h,l,[,]")
 --
 -- Setup formatters for JavaScript family
 --
-lvim.lang.javascript.formatters = {
-  {
-    exe = "prettier",
-    args = lvim.lang.javascript.formatters[1].args,
-  },
-}
-lvim.lang.typescript.formatters = lvim.lang.javascript.formatters
-lvim.lang.javascriptreact.formatters = lvim.lang.javascript.formatters
-lvim.lang.typescriptreact.formatters = lvim.lang.javascript.formatters
-lvim.lang.vue.formatters = lvim.lang.javascript.formatters
+-- lvim.lang.javascript.formatters = {
+--   {
+--     exe = "prettier",
+--     args = lvim.lang.javascript.formatters[1].args,
+--   },
+-- }
+-- lvim.lang.typescript.formatters = lvim.lang.javascript.formatters
+-- lvim.lang.javascriptreact.formatters = lvim.lang.javascript.formatters
+-- lvim.lang.typescriptreact.formatters = lvim.lang.javascript.formatters
+-- lvim.lang.vue.formatters = lvim.lang.javascript.formatters
 
 --
 -- Setup linters for JavaScript family
 --
-lvim.lang.javascript.linters = { { exe = "eslint" } }
-lvim.lang.typescript.linters = lvim.lang.javascript.linters
-lvim.lang.javascriptreact.linters = lvim.lang.javascript.linters
-lvim.lang.typescriptreact.linters = lvim.lang.javascript.linters
-lvim.lang.vue.linters = lvim.lang.javascript.linters
+-- lvim.lang.javascript.linters = { { exe = "eslint" } }
+-- lvim.lang.typescript.linters = lvim.lang.javascript.linters
+-- lvim.lang.javascriptreact.linters = lvim.lang.javascript.linters
+-- lvim.lang.typescriptreact.linters = lvim.lang.javascript.linters
+-- lvim.lang.vue.linters = lvim.lang.javascript.linters
 
 -- CSS formatter
 lvim.lang.css.formatters = {{ "prettier" }}
@@ -43,6 +43,6 @@ lvim.lang.css.formatters = {{ "prettier" }}
 -- PHP linter & formatter
 -- TODO: get phpcs to run when file is saved
 -- TODO: run phpcbf manually
-lvim.lang.php.linters = {{ exe = "phpcs" }}
-lvim.lang.php.formatters = {{ exe = "phpcbf" }}
+-- lvim.lang.php.linters = {{ exe = "phpcs" }}
+-- lvim.lang.php.formatters = {{ exe = "phpcbf" }}
 
