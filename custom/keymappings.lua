79ada0c297b12ccae07bef8af8114c38143850db
Joan López <juanlopez4691@gmail.com>
2022-04-02 11:20:51 +0200

    Fixed use of lsp deprecated document and workspace diagnostics methods.

--- custom/keymappings.lua
+++ custom/keymappings.lua
@@ -38,9 +38,9 @@ lvim.builtin.which_key.mappings["t"] = {
   r = { "<cmd>Trouble lsp_references<cr>", "References" },
   t = { "<cmd>TroubleToggle<CR>", "Trouble toggle" },
   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-  d = { "<cmd>Trouble lsp_document_diagnostics<cr>", "Diagnosticss" },
+  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnosticss" },
   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-  w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnosticss" },
+  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordkspace Diagnosticss" },
 }
 
