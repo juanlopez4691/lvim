7979acf8aaf538d383c70b88a5990113bd772ea0
Joan López <juanlopez4691@gmail.com>
2023-01-28 18:08:47 +0100

    Ensured treesitter used bash highlighting for zsh files.

--- lua/custom/autocommands.lua
+++ lua/custom/autocommands.lua
@@ -1,4 +1,5 @@
 -- ## Autocommands
+-- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
 
 lvim.autocommands = {
   {
@@ -10,3 +11,14 @@ lvim.autocommands = {
     },
   }
 }
+
+vim.api.nvim_create_autocmd(
+  "FileType",
+  {
+    pattern = "zsh",
+    callback = function()
+      -- let treesitter use bash highlight for zsh files as well
+      require("nvim-treesitter.highlight").attach(0, "bash")
+    end,
+  }
+)
