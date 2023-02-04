1e2550549ce6515551fb072c61a491e2a4ccebd5
Joan López <juanlopez4691@gmail.com>
2023-02-04 18:47:09 +0100

    Disabled plugin SmiteshP/nvim-navic.

--- lua/custom/plugins/core.lua
+++ lua/custom/plugins/core.lua
@@ -1,4 +1,7 @@
 -- ## Core plugins settings
 
+-- Disable SmiteshP/nvim-navic plugin
+lvim.builtin.breadcrumbs.active = false
+
 -- Show git icons in nvimtree
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
