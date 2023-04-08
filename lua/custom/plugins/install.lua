484a1230ff3611266efa8287c9b35058c349cf97
Joan López <juanlopez4691@gmail.com>
2023-04-08 12:39:47 +0200

    Added plugin danymat/neogen to generate annotations for classes, methods.

--- lua/custom/plugins/install.lua
+++ lua/custom/plugins/install.lua
@@ -46,6 +46,13 @@ lvim.plugins = {
     end
   },
   {
+    'danymat/neogen',
+    dependencies = 'nvim-treesitter/nvim-treesitter',
+    config = true,
+    -- Uncomment next line if you want to follow only stable versions
+    version = '*'
+  },
+  {
     'simrat39/symbols-outline.nvim',
     config = function()
       require('symbols-outline').setup({
