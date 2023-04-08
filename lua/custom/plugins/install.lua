af27e7955b12cc43b533ff131062a9f1f896fea5
Joan López <juanlopez4691@gmail.com>
2023-04-08 12:41:03 +0200

    Added plugin nvim-tree/nvim-web-devicons.

--- lua/custom/plugins/install.lua
+++ lua/custom/plugins/install.lua
@@ -230,4 +230,14 @@ lvim.plugins = {
     'willothy/flatten.nvim',
     config = true,
   },
+  -- Better file icons
+  {
+    "nvim-tree/nvim-web-devicons",
+    dependencies = { "DaikyXendo/nvim-material-icon" },
+    config = function()
+      require("nvim-web-devicons").setup {
+        override = require("nvim-material-icon").get_icons(),
+      }
+    end,
+  },
 }
