9c2716a6bcf8daa6f4ce131464148f95263e941e
Joan López <juanlopez4691@gmail.com>
2023-04-08 16:12:00 +0200

    Added lazy loading to plugin nvim-tree/nvim-web-devicons.

--- lua/custom/plugins/install.lua
+++ lua/custom/plugins/install.lua
@@ -236,6 +236,7 @@ lvim.plugins = {
   -- Better file icons
   {
     "nvim-tree/nvim-web-devicons",
+    lazy = true,
     dependencies = { "DaikyXendo/nvim-material-icon" },
     config = function()
       require("nvim-web-devicons").setup {
